2253ce6d820daf22a575097b539892cf64a41b7e
RAM SINGH VERMA <123285357+Ram-verma-793@users.noreply.github.com>
2025-07-02 13:39:24 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,10 @@
+name = input("Enter your name : ")
+print(name)
+print(type(name))  # the input function always take input as string even we give it a integer value or any other 
+                   # to change it we use type casting
+                   # converting one to another datatype is called typecasting
+
+
+rollNo = int(input("Enter roll no. : "))
+print(rollNo)
+print(type(rollNo))
---
+++ sum_of_two_input.py
@@ -0,0 +1,5 @@
+a = int(input("Enter first digit : "))
+b = int(input("Enter second digit : "))
+
+
+print("result : ", a+b)
